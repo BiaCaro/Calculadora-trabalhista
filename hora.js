058fa93d7265ea160dcc1d4059adef1ad38c3e6c
BiaCaroline <ext_biancaro@mercadolivre.com>
2021-08-30 12:40:20 -0300

    Add files via upload

--- hora.js
+++ hora.js
@@ -1,20 +1,23 @@
-// var salario = document.getElementById('tx1')
-// var cargahora = document.getElementById('tx2')
-// var resultado = document.getElementById('result')
 
-window.alert('Bem vindo! Essa é a super calculadora trabalhista da Bianca! ')
+window.alert('Bem vindo! Essa é a super calculadora trabalhista da Bianca!')
+var peg = window.prompt('Está preparado?')
 
-
+    if (peg.toUpperCase() !== 'SIM') {
+        window.alert('Poxa, como assim?')
+        location.reload()
+    } else { window.alert('Então vamos lá') }
 
 
 
 function dividir(){
-    var salario = document.getElementById('tx1')
-    var cargahora = document.getElementById('tx2')
-    var resultado = document.getElementById('results')
 
-    var n1 = Number(salario.value)
-    var n2 = Number(cargahora.value)
-    var d = n1 / n2
+    let salario = document.getElementById('tx1') // tentei usar o let fora da função, mas não funcionou, não entendi pq :/
+    let cargahora = document.getElementById('tx2')
+    let resultado = document.getElementById('results')
+
+    let n1 = Number(salario.value)
+    let n2 = Number(cargahora.value)
+    let d = n1 / n2
     resultado.innerHTML = `Seu salário/hora é de R$ ${d} reais`
 }
+
